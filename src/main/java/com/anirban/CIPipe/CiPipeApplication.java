ed7ab66e5432910df1c265f01a18481e9f506a86
ianirbanc <ianirbaanc@gmail.com>
2021-02-27 16:20:20 +0000

    Commit for CI Pipe

---
+++ src/main/java/com/anirban/CIPipe/CiPipeApplication.java
@@ -0,0 +1,13 @@
+package com.anirban.CIPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anirban/CIPipe/CiPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirban.CIPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
